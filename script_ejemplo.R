5aa1343f9d706c5b5d4b13b553e18dcc8e11a121
sarixxxxx <s.gonzalezd@uniandes.edu.co>
2024-08-15 08:43:55 -0500

    archivos en carpeta

---
+++ script_ejemplo.R
@@ -0,0 +1,24 @@
+# This is a comment:
+# this is my first R script
+
+# Print something in the console
+print("Hello World") 
+
+# do some things
+x = 34
+y = 16
+z = x + y   # addition
+w = y/x     # division
+# display the results
+x
+y
+z
+w
+# change x
+x = "some text"
+# display the results
+x
+y
+z
+w
+
